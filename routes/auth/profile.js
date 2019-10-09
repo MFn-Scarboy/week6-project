3004e96fed10c822444e846d3aa5373aa3bd6d9f
nehaarora <neha95218@gmail.com>
2019-10-09 16:33:31 +0200

    final preview

--- routes/auth/profile.js
+++ routes/auth/profile.js
@@ -2,7 +2,6 @@ const express = require("express")
 const router = express.Router()
 const plan = require("../../models/plan")
 
-//needs to be restricted
 router.get("/profile", (req, res, next) => {
    let loggedInUser = req.session.user
     plan.findById(loggedInUser.plan)
