57b6af5b315108f77cc717dbf9432da54b5da0be
joel <jrv3rkaik@gmail.com>
2019-10-13 19:56:00 +0200

    finishline

--- routes/auth/profile.js
+++ routes/auth/profile.js
@@ -5,7 +5,6 @@ const plan = require("../../models/plan")
 
 router.get("/profile", (req, res, next) => {
    let loggedInUser = req.session.user
-   console.log(req.session.user)
     plan.findById(loggedInUser.plan)
     .then((plan)=>{
         res.render("profile", {loggedInUser, plan});
